25ba6674331a03f0846dd87178db8945af6bd505
waspryo <wasp2123321@gmail.com>
2020-04-11 21:49:30 +0900

    v-model

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <top></top>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import top from './components/top.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    top
   }
 }
 </script>
---
+++ src/components/top.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="top-text">{{greeting}}</div>
+    <input v-model="greeting">
+  </div>
+</template>
+
+<script>
+  export default {
+    data: function () {
+      return {
+        greeting: 'nice'
+      }
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+  
+</style>
